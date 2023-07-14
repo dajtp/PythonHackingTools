ad1e78e074546d256a260d735583b6fbf5000199
Jake Parry <jakeparry@email.com>
2023-07-14 04:39:28 +0100

    Update

--- Basic Networking Tools/netcat.py
+++ Basic Networking Tools/netcat.py
@@ -1,3 +1,5 @@
+# Import necerssary libraries
+
 import argparse
 import socket
 import shlex
@@ -6,9 +8,12 @@ import sys
 import textwrap
 import threading
 
+# Create a function (execute) which receives a command, runs it and then returns the output as a string. Uses subprocess' check_output method which runs a command on the local system OS and then returns the output.
+
 def execute(cmd):
     cmd = cmd.strip()
     if not cmd:
         return
     output = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
     return output.decode()
+
