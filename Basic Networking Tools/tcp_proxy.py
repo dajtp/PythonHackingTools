30ea5870b052e5326f57b59eafd59caeceb465cc
Jake Parry <jakeparry@email.com>
2023-07-15 01:40:20 +0100

    Update

--- Basic Networking Tools/tcp_proxy.py
+++ Basic Networking Tools/tcp_proxy.py
@@ -1,3 +1,9 @@
+# A few imports to start. 
+# I created a HEXFILTER string that contains ASCII printable characters, if one exists, or a dot (.) if no representation exists.
+# I defined a hexdump function that input as bytes or a strong and prints a hexdump to the console. I.e. It will output packet details with both their hexadecimal values and ASCII printable characters.
+# This is useful for understanding unknown protocols, finding creds in plaintext protocols and more.
+
+
 import sys
 import socket
 import threading
@@ -21,3 +27,58 @@ def hexdump(src, length=16, show=True):
             print(line)
     else:
         return results
+    
+#   For receiving both local and remote data, we pass in the socket object to be used. We create an empty byte string (buffer) that will accumulate responses from the socket. 
+#   ((35)) - By default, we set a 5 second timeout. This may need to be adjusted if we are proxying over a poor connection or internationally etc. 
+#   ((40)) - We setup a loop to read response data into the buffer until there is no more data left or we timeout.
+#   ((47)) - Finally, we return the byte buffer string to the caller. which could be either a remote or local machine. 
+
+def receive_from(connection):
+    buffer = b""
+    connection.settimeout(5)
+    try:
+        while True:
+            data = connection.recv(4096)
+            if not data:
+                break
+            buffer += data
+    except Exception as e:
+        pass
+    return buffer
+
+def request_handler(buffer):
+    # Perform packet modifications
+    return buffer
+
+def response_handler(buffer):
+    # Perform packet modifications
+    return buffer
+
+def proxy_handler(client_socket, remote_host, remote_port, receive_first):
+    remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    remote_socket.connect((remote_host, remote_port))
+    
+    if receive_first:
+        remote_buffer = receive_from(remote_socket)
+        hexdump(remote_buffer)
+    
+    remote_buffer = response_handler(remote_buffer)
+    if len(remote_buffer):
+        print("[<==] Sending %d bytes to localhost." % len(remote_buffer))
+        client_socket.send(remote_buffer)
+    
+    while True:
+        local_buffer = receive_from(client_socket)
+        if len(local_buffer):
+            line = "[==>]Received %d bytes from localhost." % len(local_buffer)
+            print(line)
+            hexdump(local_buffer)
+            
+            local_buffer = request_handler(local_buffer)
+            remote_socket.send(local_buffer)
+            print("[<==] Sent to remote.")
+        
+        remote_buffer = receive_from(remote_socket)
+        if len(remote_buffer):
+            print("[<==] Received %d bytes from remote" % len(remote_buffer))
+            
