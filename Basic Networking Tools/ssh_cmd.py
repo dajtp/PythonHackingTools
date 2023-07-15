b1ec4b8b89ec4e7786d15147e04dfde72d938421
Jake Parry <jakeparry@email.com>
2023-07-15 05:33:26 +0100

    New File

---
+++ Basic Networking Tools/ssh_cmd.py
@@ -0,0 +1,4 @@
+import paramiko
+
+# Paramiko GitHub: https://github.com/paramiko/paramiko
+# Paramiko is a Python produced SSHv2 Protocol. We can use this to build an SSH Client on a system that may not have SSH present. (I.e - Think Windows without privs)
