511d6f44f750c8b2040324f31ad21a4a2a9f95aa
Jake <105429229+dajtp@users.noreply.github.com>
2023-08-09 23:01:24 +0100

    New File

--- Basic Networking Tools/ssh_paramiko.py
+++ Basic Networking Tools/ssh_paramiko.py
@@ -1,2 +1,14 @@
 import paramiko
 
+def ssh_command(ip, port, user, passwd, cmd):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ip, port=port, username=user, password=passwd)
+    
+    _, stdout, stderr = client.exec_command(cmd)
+    output = stdout.readlines() + stderr.readlines()
+    if output:
+        print('---Output---')
+        for line in output:
+            print(line.strip())
+            
